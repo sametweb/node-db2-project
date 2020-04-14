75b488867c1c0e7e0b7fe2b273dec5839c09b377
sametweb <sametmutevellioglu@gmail.com>
2020-04-14 14:57:11 -0700

    update/delete endpoints

--- cars/carsRouter.js
+++ cars/carsRouter.js
@@ -29,11 +29,43 @@ router.post("/", (req, res) => {
             .then((car) => res.status(201).json(car))
         : res.status(500).json({ error: "error saving the car to db", err });
     })
+    .catch((err) => res.status(500).json({ error: "error saving the record" }));
+});
+
+router.patch("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+  db("cars")
+    .update(changes)
+    .where({ id })
+    .then((updated) => {
+      updated
+        ? db("cars")
+            .where({ id })
+            .first()
+            .then((updatedCar) => res.status(200).json(updatedCar))
+        : res.status(400).json({ error: "couldn't find a record with the id" });
+    })
     .catch((err) =>
-      res.status(500).json({ error: "Error adding the car to the database." })
+      res.status(500).json({ error: "error updating the record", err })
     );
 });
 
-router.patch("/");
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  db("cars")
+    .del()
+    .where({ id })
+    .then((deleted) =>
+      deleted
+        ? res.status(204).end()
+        : res
+            .status(404)
+            .json({ error: "the record with specified id couldn't be found" })
+    )
+    .catch((err) =>
+      res.status(500).json({ error: "error deleting the record", err })
+    );
+});
 
 module.exports = router;
