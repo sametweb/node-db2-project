b4f3814e8a8e0799940f1a0c57a0124cfda0c374
sametweb <sametmutevellioglu@gmail.com>
2020-04-14 14:41:57 -0700

    seeds added

---
+++ data/seeds/01_cars.js
@@ -0,0 +1,34 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex("cars")
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex("cars").insert([
+        {
+          VIN: "ASDFGHWERTYU12345",
+          make: "Mercedes",
+          model: "E180",
+          milage: 125000,
+          transmission: "auto",
+          title_status: "clean",
+        },
+        {
+          VIN: "ASDFGHWERTYU1A345",
+          make: "BMW",
+          model: "X5",
+          milage: 75000,
+          transmission: "manuel",
+          title_status: "clean",
+        },
+        {
+          VIN: "ASDFGHWERAAU12345",
+          make: "Volkswagen",
+          model: "Passat",
+          milage: 42500,
+          transmission: "DSG",
+          title_status: "salvage",
+        },
+      ]);
+    });
+};
